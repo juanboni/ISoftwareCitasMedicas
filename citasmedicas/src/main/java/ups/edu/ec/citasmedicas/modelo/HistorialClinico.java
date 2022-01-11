b2b90de700373102c313e543fdc1f49515e4da39
juanboni <52544365+juanboni@users.noreply.github.com>
2022-01-11 18:03:32 -0500

    create historial

--- citasmedicas/src/main/java/ups/edu/ec/citasmedicas/modelo/HistorialClinico.java
+++ citasmedicas/src/main/java/ups/edu/ec/citasmedicas/modelo/HistorialClinico.java
@@ -3,6 +3,7 @@ package ups.edu.ec.citasmedicas.modelo;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -14,9 +15,10 @@ import javax.persistence.Table;
 @Table(name = "TBL_HistorialClinico")
 public class HistorialClinico {
 	@Id
+	@Column(name = "hist_id")
 	private int id;
 	@OneToOne
-	@JoinColumn(name = "id")
+	@JoinColumn(name = "con_id")
 	private ConsultaMedica consultasmedicas;
 	public int getId() {
 		return id;
